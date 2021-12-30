240d9c5147b47729dc4e715c61671b3215a5c63f
Your Name <you@example.com>
2021-12-30 23:43:58 +0200

    filling with methods of the BookService class

--- src/main/java/com/example/bookservice/service/BookService.java
+++ src/main/java/com/example/bookservice/service/BookService.java
@@ -1,4 +1,79 @@
 package com.example.bookservice.service;
 
+import com.example.bookservice.model.Author;
+import com.example.bookservice.model.Book;
+import com.example.bookservice.model.Publisher;
+import com.example.bookservice.repository.AuthorRepository;
+import com.example.bookservice.repository.BookRepository;
+import com.example.bookservice.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> saveBooks(List<Book> books) {
+        return bookRepository.saveAll(books);
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public List<Book> getBooksByName(String name) {
+        return bookRepository.findAllByName(name);
+    }
+
+    public List<Book> getBooksByCreationDate(Date date) {
+        return bookRepository.findAllByCreationDate(date);
+    }
+
+    public List<Book> getBooksByPages(int pages) {
+        return bookRepository.findAllByPages(pages);
+    }
+
+    public Book updateBook(Book book) {
+        Book updatedBook = bookRepository.getById(book.getId());
+        updatedBook.setPages(book.getPages());
+        updatedBook.setName(book.getName());
+        updatedBook.setCreationDate(book.getCreationDate());
+        updatedBook.setAuthor(book.getAuthor());
+        return bookRepository.save(book);
+    }
+
+    public String deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return "Book #" + id + " was deleted!";
+    }
+
+    public Book addBookToAuthor(Long author_id, Long book_id) {
+        Book book = bookRepository.findById(book_id).orElse(null);
+        Author author = authorRepository.findById(author_id).orElse(null);
+        book.assignAuthor(author);
+        return bookRepository.save(book);
+    }
+
+    public Book addBookToPublisher(Long publisher_id, Long book_id) {
+        Book book = bookRepository.findById(book_id).orElse(null);
+        Publisher publisher = publisherRepository.findById(publisher_id).orElse(null);
+        book.assignPublisher(publisher);
+        return bookRepository.save(book);
+    }
 }
