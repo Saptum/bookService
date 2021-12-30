2b6a0ab05c47b4cf0ee72e2af13474b1d4c72f2b
Your Name <you@example.com>
2021-12-30 23:23:50 +0200

    added methods

--- src/main/java/com/example/bookservice/service/PublisherService.java
+++ src/main/java/com/example/bookservice/service/PublisherService.java
@@ -1,4 +1,62 @@
 package com.example.bookservice.service;
 
+import com.example.bookservice.model.Publisher;
+import com.example.bookservice.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PublisherService {
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    public Publisher savePubliser(Publisher publisher) {
+        return publisherRepository.save(publisher);
+    }
+
+    public List<Publisher> saveAllPublisher(List<Publisher> publishers) {
+        return publisherRepository.saveAll(publishers);
+    }
+
+    public List<Publisher> getPublishers() {
+        return publisherRepository.findAll();
+    }
+
+    public Publisher getPublisherById(Long id) {
+        return publisherRepository.findById(id).orElse(null);
+    }
+
+    public Publisher getPublisherByName(String name) {
+        return publisherRepository.findByName(name);
+    }
+
+    public List<Publisher> getPublishersByCountry(String country) {
+        return publisherRepository.findAllByCountry(country);
+    }
+
+    public List<Publisher> getPublishersByCity(String city) {
+        return publisherRepository.findAllByCity(city);
+    }
+
+    public Publisher getPublisherByAddress(String address) {
+        return publisherRepository.findByAddress(address);
+    }
+
+    public Publisher updatePublisher(Publisher publisher) {
+        Publisher updatedPublisher = publisherRepository.findById(publisher.getId()).orElse(null);
+        updatedPublisher.setCity(publisher.getCity());
+        updatedPublisher.setCountry(publisher.getCountry());
+        updatedPublisher.setAddress(publisher.getAddress());
+        updatedPublisher.setName(publisher.getName());
+        updatedPublisher.setBooks(publisher.getBooks());
+        return publisherRepository.save(updatedPublisher);
+    }
+
+    public String deletePublisher(Long id) {
+        publisherRepository.deleteById(id);
+        return "Publisher #" + id + " was deleted!";
+    }
+
 }
