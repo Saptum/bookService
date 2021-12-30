b10d1e775fa804105dc3eb7945d7f11488f4f98e
Your Name <you@example.com>
2021-12-30 14:43:22 +0200

    filling the AuthorService class with new methods

--- src/main/java/com/example/bookservice/service/AuthorService.java
+++ src/main/java/com/example/bookservice/service/AuthorService.java
@@ -1,4 +1,31 @@
 package com.example.bookservice.service;
 
+import com.example.bookservice.model.Author;
+import com.example.bookservice.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public Author findById(Long id){
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public List<Author> findAll(){
+        return authorRepository.findAll();
+    }
+
+    public Author saveAuthor (Author author){
+        return authorRepository.save(author);
+    }
+
+    public void deleteById(Long id){
+        authorRepository.deleteById(id);
+    }
 }
