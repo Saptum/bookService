5511a13f4d3015e1502288cfba8b95742b8e9e18
Your Name <you@example.com>
2021-12-30 19:18:36 +0200

    added several methods

--- src/main/java/com/example/bookservice/controller/AuthorController.java
+++ src/main/java/com/example/bookservice/controller/AuthorController.java
@@ -5,11 +5,9 @@ import com.example.bookservice.model.Book;
 import com.example.bookservice.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -33,5 +31,44 @@ public class AuthorController {
         return authorService.findAll();
     }
 
+    @GetMapping("/authorById/{id}")
+    public Author getAuthorById(@PathVariable Long id) {
+        return authorService.findById(id);
+    }
+
+    @GetMapping("/authorByFirstName/{firstName}")
+    public List<Author> getAuthorsByFirstName(@PathVariable String firstName) {
+        return authorService.getAuthorsByFirstName(firstName);
+    }
+
+    @GetMapping("/authorByLastName/{lastName}")
+    public List<Author> getAuthorsByLastName(@PathVariable String lastName) {
+        return authorService.getAuthorsByLastName(lastName);
+    }
+
+    @GetMapping("/authorByCountry/{country}")
+    public List<Author> getAuthorsByCountry(@PathVariable String country) {
+        return authorService.getAuthorsByCountry(country);
+    }
+
+    @GetMapping("/authorBySex/{sex}")
+    public List<Author> getAuthorsBySex(@PathVariable String sex) {
+        return authorService.getAuthorsBySex(sex);
+    }
+
+    @GetMapping("/authorByBirth/{birth}")
+    public List<Author> getAuthorsByBirth(@PathVariable Date birth) {
+        return authorService.getAuthorsByBirth(birth);
+    }
+
+    @PutMapping("/updateAuthor")
+    public Author updateAuthor(@RequestBody Author author) {
+        return authorService.updateAuthor(author);
+    }
+
+    @DeleteMapping("/deleteAuthor/{id}")
+    public String deleteAuthor(@PathVariable Long id) {
+        return authorService.deleteAuthor(id);
+    }
 
 }
