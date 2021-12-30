5c66ef659ae6bd000041a997cdf506c4c5b557c3
Your Name <you@example.com>
2021-12-30 19:08:40 +0200

    added several methods

--- src/main/java/com/example/bookservice/controller/AuthorController.java
+++ src/main/java/com/example/bookservice/controller/AuthorController.java
@@ -1,14 +1,33 @@
 package com.example.bookservice.controller;
 
+import com.example.bookservice.model.Author;
+import com.example.bookservice.model.Book;
 import com.example.bookservice.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class AuthorController {
 
     @Autowired
     private AuthorService authorService;
+
+    @PostMapping("/saveAuthor")
+    public Author saveAuthor(@RequestBody Author author) {
+        return authorService.saveAuthor(author);
+    }
+
+public Author addBook(@PathVariable Long id,@RequestBody Book book){
+        return authorService.addBook(book, id);
+}
+
+
+
 
 
 }
